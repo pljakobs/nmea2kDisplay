9d84cad7975a08d017e67533117ba0861e904758
Peter Jakobs <pjakobs@mobileme.de>
2022-01-12 23:07:54 +0100

    masthead now sends a complete nmea sentence rather than single digits

--- BT_Masthead/BT_Masthead.ino
+++ BT_Masthead/BT_Masthead.ino
@@ -11,19 +11,49 @@
 #endif
 
 BluetoothSerial SerialBT;
+char sentence[80];
+typedef enum {OUT, IN, FIN1, FIN2 } state_t;
+int i;
 
+state_t state;
 void setup() {
+ state=OUT;
  Serial.begin(4800);
  SerialBT.begin("AntaresMasthead"); //Bluetooth device name // <------- set this to be the same as the name you chose above!!!!!
  //Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
+  char c;
+ 
   if (Serial.available()) {
-    SerialBT.write(Serial.read());
+    c=Serial.read();
+    switch(c){
+      case '$':
+        if(state==OUT){
+            state=IN;
+         }else{
+            sentence[i++]=c;
+         }
+         break;
+      case 0x0d:
+        if(state==IN){
+          state=FIN1;
+        }
+        break;
+      case 0x0a:
+        if(state==FIN1){
+          state=FIN2;
+          sentence[i++]=0x0;
+          //SerialBT.printf("got %i bytes",i);
+          SerialBT.printf("$%s\r\n",&sentence);
+          i=0;
+          state=OUT;
+        }
+        break;
+      default:
+        //SerialBT.printf("got char: %c",c);
+        sentence[i++]=c;
+    }
   }
-//  if (SerialBT.available()) {
-//    Serial.write(SerialBT.read());
-//  }
-
 }
